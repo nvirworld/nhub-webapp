8f167d05a0fb27ae9bd8adfdb5cd12eb636bc415
bsangs <tkdals6805@naver.com>
2024-08-07 11:07:01 +0900

    replace network filter host

--- src/hooks/useQueryNetworks.ts
+++ src/hooks/useQueryNetworks.ts
@@ -1,18 +1,20 @@
-import { useQuery } from '@tanstack/react-query'
-import { getNetworks } from '../services/api.service'
+import { useQuery } from "@tanstack/react-query";
+import { getNetworks } from "../services/api.service";
 
 export const useQueryNetworks = () => {
   return useQuery({
-    queryKey: ['Networks'],
+    queryKey: ["Networks"],
     queryFn: async () => {
-      const networks = await getNetworks()
+      const networks = await getNetworks();
 
-      const host = window.location.host
-      if (host === 'v2.n-hub.io') {
-        return networks.filter((network) => (network.chainId !== 5 && network.chainId !== 11155111))
+      const host = window.location.host;
+      if (host.includes("nuralchain.org")) {
+        return networks.filter(
+          (network) => network.chainId !== 5 && network.chainId !== 11155111
+        );
       }
-      return networks
+      return networks;
     },
-    staleTime: 1000 * 60
-  })
-}
+    staleTime: 1000 * 60,
+  });
+};
